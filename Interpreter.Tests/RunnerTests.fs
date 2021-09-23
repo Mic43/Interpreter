3a051feaed5bff6830e4283bf5ae8371cd26307e
Michał <m>
2021-09-23 11:11:46 +0200

    Changed structure of the applcation

---
+++ Interpreter.Tests/RunnerTests.fs
@@ -0,0 +1,94 @@
+﻿module RunnerTests
+
+open Xunit
+open Interpreter.AST
+open FsCheck
+open FsCheck.Xunit
+open FSharpPlus
+
+module Run =
+    let (.=.) left right =
+        left = right |@ sprintf "%A = %A" left right
+
+    let idFuncProgram inputVal =
+        let varIdent = Identifier.createIdentifier "x"
+        let funIdent = Identifier.createIdentifier "id"
+        // let inputVal = input |> IntValue
+        let actualParam = inputVal |> Constant
+
+        let idFunDecl =
+            { Name = funIdent
+              Parameters = [ varIdent ]
+              Body = [ varIdent |> Var |> ExpressionStatement ] }
+
+        [ idFunDecl |> FunDeclaration
+          { Name = funIdent
+            ActualParameters = [ actualParam ] }
+          |> FunCall
+          |> ExpressionStatement
+          |> ScopedStatement ]
+        |> Program
+
+    [<Property>]
+    let ``identity function returns input when called with int argument`` (input: int) =
+        let inputVal = input |> IntValue
+
+        let sut = new Runner()
+
+        let actual = sut.Run(idFuncProgram inputVal)
+
+        let expected = inputVal |> Ok
+
+        actual .=. expected
+
+    [<Property>]
+    let ``identity function returns input when called with float argument`` (input: NormalFloat) =
+        let inputVal = input |> float |> FloatValue
+
+        let sut = new Runner()
+
+        let actual = sut.Run(idFuncProgram inputVal)
+
+        let expected = inputVal |> Result.Ok
+
+        actual .=. expected
+
+    [<Property>]
+    let ``There can be only one function with specific name``
+        (funName: NonEmptyString)
+        (paramName: NonEmptyString)
+        input
+        (len: PositiveInt)
+        =
+        let varIdent =
+            Identifier.createIdentifier (paramName |> string)
+
+        let funIdent =
+            Identifier.createIdentifier (funName |> string)
+
+        let inputVal = input |> IntValue
+        let actualParam = inputVal |> Constant
+
+        let idFunDecl =
+            { Name = funIdent
+              Parameters = [ varIdent ]
+              Body = [ varIdent |> Var |> ExpressionStatement ] }
+
+        let program =
+            ((fun _ -> idFunDecl |> FunDeclaration)
+             |> List.init (len.Get + 1))
+            @ [ { Name = funIdent
+                  ActualParameters = [ actualParam ] }
+                |> FunCall
+                |> ExpressionStatement
+                |> ScopedStatement ]
+            |> Program
+
+        let sut = new Runner()
+
+        let actual = sut.Run(program)
+
+        (match actual with
+         | Ok _ -> false
+         | Error _ -> true)
+    
---
+++ Interpreter/AST/Types.fs
@@ -0,0 +1,115 @@
+﻿namespace Interpreter.AST
+
+open System
+open FSharpPlus
+
+type Value =
+    | IntValue of int
+    | FloatValue of float
+    | VoidValue of unit
+
+module Value =
+    let toFloat =
+        function
+        | IntValue v -> v |> float
+        | FloatValue v -> v
+        | _ -> invalidArg "val" "cannot convert void to float"
+
+    let private compute opFloat opInt v1 v2 =
+        match (v1, v2) with
+        | (IntValue v1, IntValue v2) -> (opInt v1 v2) |> IntValue |> Result.Ok
+        | (VoidValue _, _)
+        | (_, VoidValue _) ->
+            "cannot convert void to float"
+            |> (Errors.createResult Other)
+        | (v1, v2) ->
+            (opFloat (v1 |> toFloat) (v2 |> toFloat))
+            |> FloatValue
+            |> Result.Ok
+
+    let private computeBool opFloat opInt v1 v2 : bool =
+        match (v1, v2) with
+        | (IntValue v1, IntValue v2) -> (opInt v1 v2)
+        | (v1, v2) -> (opFloat (v1 |> toFloat) (v2 |> toFloat))
+
+    let (+) v1 v2 = compute (+) (+) v1 v2
+    let (-) v1 v2 = compute (-) (-) v1 v2
+    let (*) v1 v2 = compute (*) (*) v1 v2
+    let (/) v1 v2 = compute (/) (/) v1 v2
+
+    let Void = () |> VoidValue
+    let createVoid () = Void
+//let (>) v1 v2 = computeBool (>) (>) v1 v2
+
+
+type Identifier = private Identifier of string
+
+module Identifier =
+    let tryCreateIdentifier s = Identifier s
+    let createIdentifier s = (tryCreateIdentifier s)
+
+    let toStr =
+        function
+        | Identifier s -> s
+
+type UnaryOp =
+    | Negate
+    | Minus
+
+type BinaryArithmeticOp =
+    | Add
+    | Sub
+    | Mul
+    | Div
+
+type BinaryLogicalOp =
+    | And
+    | Or
+
+type BinaryRelationalOp =
+    | Greater
+    | Less
+    | Equal
+
+type BinaryOp =
+    | ArithmeticOp of BinaryArithmeticOp
+    | RelationalOp of BinaryRelationalOp
+    | LogicalOp of BinaryLogicalOp
+
+type Expression =
+    | Binary of
+        {| BinaryOp: BinaryOp
+           LeftOperand: Expression
+           RightOperand: Expression |}
+    | Unary of UnaryOp * Expression
+    | Assignment of Identifier * Expression
+    | FunCall of FunCall
+    | Var of Identifier
+    | Constant of Value
+
+and FunCall =
+    { Name: Identifier
+      ActualParameters: Expression list }
+
+type VarDeclaration =
+    { Name: Identifier
+      Initializer: Expression }
+
+type Function =
+    { Name: Identifier
+      Parameters: Identifier list
+      Body: Block }
+
+and Block = ScopedStatement list
+
+and ScopedStatement =
+    | ExpressionStatement of Expression
+    | VarDeclaration of VarDeclaration
+    | PrintStatement of Expression
+    | Block of Block
+
+type Statement =
+    | FunDeclaration of Function
+    | ScopedStatement of ScopedStatement
+
+type Program = Program of Statement list
